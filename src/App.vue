3a8969db75b3efc6d77fb5929bdfdbfebb3d5117
gcabad <gonzalocabad@live.com>
2022-12-22 19:56:13 -0300

    Se realiza el final de NT2

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ConversorTemp />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ConversorTemp from './components/ConversorTemp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ConversorTemp
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/ConversorTemp.vue
@@ -0,0 +1,69 @@
+<template>
+
+  <section class="src-components-conversor-temp">
+    <h1>Conversor de Temperatura</h1>
+    <hr>
+    <p>Ingrese Temperatura °C <input type="text" v-model="celsius" ></p>
+    <br>
+    <div :style="getColor(this.celsius)"> Conv Temperatura °F {{ calcularTemp.fahrenheit }} </div>
+    <div :style="getColor(this.celsius)">Conv Temperatura °K {{ calcularTemp.kelvin }} </div>
+
+    <p>Respuestas: 1:c 2:b 3:c 4:a 5:b </p>
+
+  </section>
+
+</template>
+
+<script>
+
+  export default  {
+    name: 'src-components-conversor-temp',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+        celsius: '',
+      }
+    },
+    methods: {
+      inputValido(input) {
+        if(input.length == 0 || isNaN(input)) return false
+        else return true
+      },
+      getColor(input) {
+        let resultado = 'black'
+        if(this.inputValido(input)){
+          let temp = Number(this.celsius)
+          if(temp<=0){
+            resultado='blue'
+          }else if (temp > 0 && temp < 15){
+            resultado= 'magenta'
+          }else{
+            resultado='red'
+          }
+        } 
+        return {color: resultado}
+
+      }
+    },
+    computed: {
+      calcularTemp() {
+        let temp = null
+        if(!this.inputValido(this.celsius)) return { farenheit: '', kelvin: ''}
+        else temp = Number(this.celsius)
+        return {
+          fahrenheit: (temp * (9/5) + 32),
+          kelvin: (temp + 273.15)
+        }
+      },
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+
+</style>
